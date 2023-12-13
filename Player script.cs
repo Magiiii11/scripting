1945efa9e07bb445dfe3a1ff9004c5bf04256ffa
Magiiii11 <150952572+Magiiii11@users.noreply.github.com>
2023-12-13 18:53:43 +0100

    Add files via upload

---
+++ Player script.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    public int playerDamge = 2;
+
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position.speed * Time.deltaTime);
+
+
+        transform.LookAt(target.position);
+    }
+
+        private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<player>();
+
+        player.TakeDamge(playerDamge);
+    }
+}
